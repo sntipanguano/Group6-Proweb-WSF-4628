7521e82a445dd6a8103a1a740a800c5c6dab607b
Samantha Nicole Tipanguano Santamaria <sntipanguano@espe.edu.ec>
2022-08-20 22:00:31 -0500

    Login

---
+++ 05-Code/Unit3/Pharmacy/js/scriptlogin.js
@@ -0,0 +1,27 @@
+const user = document.getElementById("user");
+const password = document.getElementById("password");
+const form = document.getElementById("form");
+const parrafo = document.getElementById("warnings");
+form.addEventListener("submit", e=>{
+    e.preventDefault();
+	let regexNombre = /^[A-Za-z\s]{3,25}$/
+    var warnings = ""
+    var entrar = false;
+    parrafo.innerHTML = ""
+    if(user.value != "admin"){
+        warnings += `Usuario o contraseña no es correcto.<br>`
+        entrar = true
+    }else if(password.value != "admin"){
+        warnings += `Usuario o contraseña no es correcto.<br>`
+        entrar = true
+    }
+	
+    if(entrar){
+        parrafo.innerHTML = warnings;
+    }else{
+        window.location.href = "HTML/inicio.html";
+		console.log(user.value +" " +password.value)
+		form.reset();
+		
+	}
+});    
