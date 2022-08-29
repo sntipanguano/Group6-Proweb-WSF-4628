a99afe4ab92db1a95dac8a9e6bc871cbba013cc6
NicoleTrujilloC <netrujillo@espe.edu.ec>
2022-08-29 00:23:32 -0500

    validate

--- 05-Code/Unit3/Pharmacy/js/validate.js
+++ 05-Code/Unit3/Pharmacy/js/validate.js
@@ -1,31 +1,34 @@
 var expressions = {
 	full_name: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, // Letras y espacios, pueden llevar acentos.
+  address: /^[a-zA-Z0-9\s]{1,40}$/,
 	password: /^.{4,12}$/, // 4 a 12 digitos.
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	phone: /^\d{7,10}$/, // 7 a 10 numeros.
-  age: /^\d{1,3}$/, // 1 a 3 numeros.
+	phone: /^\d{7,10}$/,
+  priceBox: /^\d{2,6}$/,
+  age: /^\d{0-9}$/,
 };
 
 function validateName() {
   var expRegName = /^[a-zA-ZÑñÁáÉéÍíÓóÚú\s]+$/;
   var full_nameName = document.getElementById("full_name");
     if (!expRegName.exec(full_nameName.value)) {
-      alert("Only write letters.");
+      alert("En nombre escriba solo letras");
       return false;
   } else {
       return true;
   }
 }
 
-//function validateEmail() {
-  //var emailName = document.getElementById('email').value;
-  //if(!(expressions.email).test(emailName)){
-    //alert("Invalid email.");
-    //return false;
- // } else {
-   // return true;
- // }
-//}
+function validateAddress() {
+  var expReg = /^[a-zA-Z0-9-]$/;
+  var addressName = document.getElementById("address");
+    if (!expReg.exec(addressName.value)) {
+      alert("Direccion Invalida");
+      return false;
+  } else {
+      return true;
+  }
+}
 
 function validatePhone() {
   if(isNaN(document.getElementById('phone').value)){ 
@@ -36,14 +39,23 @@ function validatePhone() {
   }
 }
 
-function validateAge() {
+function validatePriceBox() {
+  if(isNaN(document.getElementById('priceBox').value)){ 
+      alert("Escriba solo numeros");
+      return false; 
+  } else {
+    return true;
+  }
+}
+
+function validateAge(age) {
   var ageName = document.getElementById('age').value;
   if(!(expressions.age).test(ageName)){
-    windows.alert("Only three digits");
+    windows.alert("Solo numeros positivos");
     return false;
   } else {
     return true;
-  }  
+  } 
 }
 
 function validatePassword() {
